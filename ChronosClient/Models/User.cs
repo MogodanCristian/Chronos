8b5bd0e83c4c5585f9c9c3104e3ad7c2cec4c329
FLORIN-GABRIEL CABAUA <79976336+CabauaFlorin@users.noreply.github.com>
2021-11-15 21:50:06 +0200

    RegisterScreen Updated

--- ChronosClient/Models/User.cs
+++ ChronosClient/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,16 @@ namespace ChronosClient.Models
 {
     class User
     {
+        public int UserId { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        public static explicit operator HttpContent(User v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
