35390abbd0e132999186b655d0b865961fe17af1
androcpp <georgeandronache.cpp@gmail.com>
2021-11-18 00:02:56 +0200

    DashboardHomeScreen Requests done

--- ChronosClient/Screens/Windows/DashboardScreen.xaml.cs
+++ ChronosClient/Screens/Windows/DashboardScreen.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ChronosClient.Models;
 using ChronosClient.Screens.Pages;
 using ChronosClient.Screens.Windows.Popups;
 
@@ -21,10 +23,23 @@ namespace ChronosClient.Screens.Windows
     /// </summary>
     public partial class DashboardScreen : Window
     {
+        public DashboardScreen(UserAuthResponse userAuth) : base()
+        {
+            //dashboardFrame.Content = new DashboardHomeScreen(userAuth);
+        }
         public DashboardScreen()
         {
             InitializeComponent();
-            dashboardFrame.Content = new DashboardHomeScreen();
+            UserAuthResponse userAuth = new UserAuthResponse
+            {
+                UserId = 14,
+                FirstName = "Florin",
+                LastName = "Cabaua",
+                DateOfBirth = DateTime.Parse("2000-07-24T00:00:00"),
+                CreatedAt = DateTime.Parse("2021-11-15T19:41:47.33"),
+                Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjE0IiwibmJmIjoxNjM3MTY0MzYwLCJleHAiOjE2Mzc3NjkxNjAsImlhdCI6MTYzNzE2NDM2MH0.T7tBkidkzFXAmqwQxYmqT-N_5Xc-vYM81aDBcg7KLiM"
+            };
+            dashboardFrame.Content = new DashboardHomeScreen(userAuth);
         }
 
         private void new_plan_button_Click(object sender, RoutedEventArgs e)
