02453e3719bf2971e1e9cae12f9990c066c90ff1
MogodanCristian <79975756+MogodanCristian@users.noreply.github.com>
2021-12-24 16:24:37 +0200

    Added the screens for the forgot password feature

---
+++ ChronosClient/Screens/Pages/ForgotPasswordScreen.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ChronosClient.Screens.Windows;
+
+namespace ChronosClient.Screens.Pages
+{
+    /// <summary>
+    /// Interaction logic for ForgotPasswordScreen.xaml
+    /// </summary>
+    public partial class ForgotPasswordScreen : Page
+    {
+        public ForgotPasswordScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void reset_code_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key== System.Windows.Input.Key.Enter && reset_code_textbox.Text=="666")
+            {
+                var mainWindow = Application.Current.MainWindow as MainWindow;
+                mainWindow.Main.Navigate(new ResetPasswordScreen());
+            }
+        }
+
+        private void reset_button_Click(object sender, RoutedEventArgs e)
+        {
+            if(reset_email.Text.Length==0)
+            {
+                MessageBox.Show("You should complete this field if you want your password recovered in this lifetime.");
+            }
+            else
+            {
+                reset_code_label.Visibility = Visibility.Visible;
+                reset_code_textbox.Visibility = Visibility.Visible;
+                //trimite mail cu kombynatzy
+
+            }
+        }
+
+        private void back_button_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = Application.Current.MainWindow as MainWindow;
+            mainWindow.Main.Navigate(new LoginScreen());
+        }
+    }
+}
