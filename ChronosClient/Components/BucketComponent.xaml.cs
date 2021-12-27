15a5ccc100220595771df8c9e07fff56c4913d87
androcpp <georgeandronache.cpp@gmail.com>
2021-12-27 21:59:37 +0200

    Load Task XML MERGED

--- ChronosClient/Components/BucketComponent.xaml.cs
+++ ChronosClient/Components/BucketComponent.xaml.cs
@@ -9,11 +9,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 using ChronosClient.Models;
 using ChronosClient.Screens.Pages;
 using ChronosClient.Screens.Windows.Popups;
@@ -23,7 +25,7 @@ namespace ChronosClient.Components
     /// <summary>
     /// Interaction logic for BucketComponent.xaml
     /// </summary>
-    public partial class BucketComponent : UserControl
+    public partial class BucketComponent : System.Windows.Controls.UserControl
     {
 
         public static readonly DependencyProperty BucketTitleProperty =
@@ -47,7 +49,7 @@ namespace ChronosClient.Components
 
         static HttpClient client = new HttpClient();
         static bool isClient = false;
-        int UserId;
+        public int UserId;
         string jwtToken;
 
 
@@ -100,6 +102,86 @@ namespace ChronosClient.Components
             Bucket bucket = new Bucket { BucketID = BucketId };
             var response = await DeleteBucketAsync(bucket);
             PlanScreen.handler.OnChanged(e);
+        }
+        static async Task<HttpResponseMessage> CreateTaskAsync(TaskCreateModel task)
+        {
+            HttpResponseMessage httpResponse = await client.PostAsJsonAsync("Tasks", task);
+            return httpResponse;
+        }
+        public async static void parseWithXmlReader(string filename, int userID, int bucketID, RoutedEventArgs e)
+        {
+            double totalOrderCost = 0.0, totalTransportCost = 0.0, avgTransportCost = 0.0;
+            int nr_comenzi = 0;
+
+            using (var xmlReader = new XmlTextReader(filename))
+            {
+                while (xmlReader.Read())
+                {
+                    if (xmlReader.NodeType == XmlNodeType.Element)
+                    {
+                        switch (xmlReader.Name)
+                        {
+                            case "Task":
+                                string title = xmlReader.GetAttribute("Title");
+                                string description = xmlReader.GetAttribute("Description");
+                                DateTime endDate = Convert.ToDateTime(xmlReader.GetAttribute("EndDate"));
+                                string priority = xmlReader.GetAttribute("Priority");
+                                int priority_in_int = 0;
+                                if (priority == "High")
+                                {
+                                    priority_in_int = 2;
+                                }
+                                else if (priority == "Medium")
+                                {
+                                    priority_in_int = 1;
+                                }
+                                else if (priority == "Low")
+                                {
+                                    priority_in_int = 0;
+                                }
+                                TaskCreateModel task = new TaskCreateModel
+                                {
+                                    Title = title,
+                                    Description = description,
+                                    StartDate = null,
+                                    EndDate = endDate,
+                                    Priority = priority_in_int,
+                                    CreatedAt = null,
+                                    Progress = 0,
+                                    BucketId = bucketID,
+                                    UserId = userID
+                                };
+                                var response = await CreateTaskAsync(task);
+                                PlanScreen.handler.OnChanged(e);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+            avgTransportCost = totalTransportCost / nr_comenzi;
+            Console.WriteLine($"Total order cost:\t{totalOrderCost}");
+            Console.WriteLine($"Total transport cost:\t{totalTransportCost}");
+            Console.WriteLine($"Average transport cost:\t{avgTransportCost}");
+        }
+        private void load_task_from_file_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "XML Files (*.xml)|*.xml";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+                    parseWithXmlReader(filePath, UserId, BucketId, e);
+                }
+            }
         }
     }
 }
